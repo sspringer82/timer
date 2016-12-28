5edf27cdbf4b50a96e8e3e73faa11b4f3650b305
Sebastian Springer <Sebastian.Springer@maibornwolff.de>
2016-12-28 18:40:01 +0100

    Animation

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ class Timer {
 
     constructor(hh, mm, ss) {
         this.interval;
+        this.animate = false;
 
         this.displayEl = document.querySelector('#display');
         this.startBtn = document.querySelector('#start');
         this.pauseBtn = document.querySelector('#pause');
         this.resetBtn = document.querySelector('#reset');
+
+        const canvas = document.querySelector('#timer');
+        this.ctx = canvas.getContext('2d');
 
         this.time = hh * 3600 + mm * 60 + ss;
         this.currentTime = this.time;
@@ -20,7 +24,27 @@ class Timer {
     }
 
     render() {
-        this.displayEl.innerHTML = this.getFormattedTime(); 
+        this.displayEl.innerHTML = this.getFormattedTime();
+    }
+
+    drawCircle() {
+        const angle = this.currentTime * 360 / this.time;
+        const start = 270;
+        const diameter = 400;
+        const radius = diameter / 2;
+        const offset = 5;
+
+        this.ctx.clearRect(0, 0, diameter, diameter);
+        this.ctx.beginPath();
+        this.ctx.arc(radius, radius, radius - offset, Math.PI / 180 * start, Math.PI / 180 * (start + angle));
+        this.ctx.lineWidth = 10;
+        this.ctx.stroke();
+
+        if (this.currentTime > 0 && this.animate) {
+            requestAnimationFrame(this.drawCircle.bind(this));
+        } else {
+            this.animate = false;
+        }
     }
 
     getFormattedTime() {
@@ -38,10 +62,16 @@ class Timer {
         this.interval = setInterval(() => {
             this.currentTime -= 1;
             this.render();
+            this.animate = true;
+            requestAnimationFrame(this.drawCircle.bind(this));
+            if (this.currentTime <= 0) {
+                clearInterval(this.interval);
+            }
         }, 1000);
     }
 
     pause() {
+        this.animate = false;
         clearInterval(this.interval);
     }
 
@@ -54,6 +84,6 @@ class Timer {
 
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
-    (new Timer(1, 2, 3)).init();
+    (new Timer(0, 20, 0)).init();
   }
 };
