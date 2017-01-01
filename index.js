29a75f90010dd67341726679271779ef7ce92f3e
Sebastian Springer <info@sebastian-springer.com>
2017-01-01 09:25:06 +0100

    Set starting values

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ class Timer {
         this.pauseBtn = document.querySelector('#pause');
         this.resetBtn = document.querySelector('#reset');
 
+        this.container = document.querySelector('#container');
+
+        this.hoursDisplay = document.querySelector('#hoursValue');
+        this.minutesDisplay = document.querySelector('#minutesValue');
+        this.secondsDisplay = document.querySelector('#secondsValue');
+
         const canvas = document.querySelector('#timer');
         this.ctx = canvas.getContext('2d');
 
@@ -20,11 +26,33 @@ class Timer {
         this.startBtn.onclick = this.start.bind(this);
         this.pauseBtn.onclick = this.pause.bind(this);
         this.resetBtn.onclick = this.reset.bind(this);
-        this.render();
+        
+        this.container.onclick = this.modifyTime.bind(this);
+
+        this.showTime();
+        this.drawCircle();
     }
 
-    render() {
-        this.displayEl.innerHTML = this.getFormattedTime();
+    modifyTime(event) {
+        const modify = JSON.parse(event.target.getAttribute('data-modify-time'));
+        if (modify === null ) {
+            return;
+        }
+        let value = 1;
+        switch (modify.type) {
+            case 'hours':
+                value = 3600;
+                break;
+            case 'minutes':
+                value = 60;
+                break;
+        }
+        if (modify.direction === 'down') {
+            value *= -1;
+        }
+        this.time += value;
+        this.currentTime += value;
+        this.showTime();
     }
 
     drawCircle() {
@@ -47,43 +75,56 @@ class Timer {
         }
     }
 
-    getFormattedTime() {
+    showTime() {
         const hh = Math.floor(this.currentTime / 3600);
         const mm = Math.floor((this.currentTime - hh * 3600) / 60);
         const ss = Math.floor(this.currentTime - hh * 3600 - mm * 60);
-        return `${this.padLeft(hh)}:${this.padLeft(mm)}:${this.padLeft(ss)}`;
+
+        this.hoursDisplay.innerHTML = this.padLeft(hh);
+        this.minutesDisplay.innerHTML = this.padLeft(mm);
+        this.secondsDisplay.innerHTML = this.padLeft(ss);
     }
 
     padLeft(value) {
         return (parseInt(value) < 10) ? '0' + value : value;
     }
 
+    toggleControls(visibility) {
+        document.querySelectorAll('[data-modify-time]').forEach((el) => {
+            el.style.visibility = visibility;
+        });
+    }
+
     start() {
+        this.toggleControls('hidden');
         this.interval = setInterval(() => {
-            this.currentTime -= 1;
-            this.render();
-            this.animate = true;
-            requestAnimationFrame(this.drawCircle.bind(this));
             if (this.currentTime <= 0) {
                 clearInterval(this.interval);
+                return;
             }
+            this.currentTime -= 1;
+            this.showTime();
+            this.animate = true;
+            requestAnimationFrame(this.drawCircle.bind(this));
         }, 1000);
     }
 
     pause() {
         this.animate = false;
+        this.toggleControls('visible');
         clearInterval(this.interval);
     }
 
     reset() {
         this.pause();
         this.currentTime = this.time;
-        this.render();
+        this.showTime();
+        this.drawCircle();
     }
 }
 
 document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
-    (new Timer(0, 20, 0)).init();
+    (new Timer(0, 0, 0)).init();
   }
 };
